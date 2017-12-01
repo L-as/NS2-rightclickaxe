f8a82e3692fdfe3dc0b165618a7458bca9e898cb
Las <las@protonmail.ch>
2017-12-01 20:47:22 +0100

    Replace Welder.lua

--- src/lua/rightclickaxe/FileHooks.lua
+++ src/lua/rightclickaxe/FileHooks.lua
@@ -2,10 +2,10 @@
 for _, v in ipairs {
 	"Builder",
 	"Axe",
-	"Welder"
 } do
 	ModLoader.SetupFileHook("lua/Weapons/Marine/" .. v .. ".lua", "lua/rightclickaxe/" .. v .. ".lua", "post")
 end
+ModLoader.SetupFileHook("lua/Weapons/Marine/Welder.lua", "lua/rightclickaxe/Welder.lua", "replace")
 
 ModLoader.SetupFileHook("lua/Marine.lua",         "lua/rightclickaxe/Marine.lua",         "post")
 ModLoader.SetupFileHook("lua/ConstructMixin.lua", "lua/rightclickaxe/ConstructMixin.lua", "post")
