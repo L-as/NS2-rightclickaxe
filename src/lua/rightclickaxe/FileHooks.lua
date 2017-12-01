b283f8e39bf5631d4bdf2eb67f6d483f60579a61
Las <las@protonmail.ch>
2017-12-01 16:21:35 +0100

    Don't remove builder class

--- src/lua/rightclickaxe/FileHooks.lua
+++ src/lua/rightclickaxe/FileHooks.lua
@@ -1,2 +1,2 @@
-ModLoader.SetupFileHook("lua/Weapons/Marine/Builder.lua", true, "halt") -- Welder replaces it completely
-ModLoader.SetupFileHook("lua/Weapons/Marine/Welder.lua", "lua/rightclickaxe/Welder.lua", "post") -- Welder replaces it completely
+ModLoader.SetupFileHook("lua/Weapons/Marine/Builder.lua", "lua/rightclickaxe/Builder.lua", "post")
+ModLoader.SetupFileHook("lua/Weapons/Marine/Welder.lua",  "lua/rightclickaxe/Welder.lua",  "post")
